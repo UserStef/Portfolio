a1fced3e7098b8ef3adfe7ea9b05da969d022b0e
UserStef <story29t@gmail.com>
2021-09-10 20:01:31 -0600

    Updating Style

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,6 @@
 'use strict';
 console.log(" -- Start of script -- ");
 var xo = new Date(Date.now());
-window.addEventListener("click", (ev) =>{
-
-    if(ev.target.dataset.navbox != null){
-        console.log(` ── Navigation box: 💠 ${ev.target.dataset.navbox} 📍 ── `);
-        let nav = document.getElementById('nav');
-        let navbox = document.getElementById('navbox');
-        if(navbox.dataset.navbox == "showing"){
-            nav.classList.add('hidden');
-            navbox.dataset.navbox = "hidding";
-            navbox.classList.remove('navbox-displaying');
-        } else {
-            navbox.classList.add('navbox-displaying');
-            nav.classList.remove('hidden');
-            navbox.dataset.navbox = "showing";
-        }
-    }
-    
-    if(ev.target.dataset.nav == "home"){
-        console.log(" ── Navigating back: 💠 Home 📍 ── ");
-        document.getElementById('welcome-msg').innerHTML = `You are already here.`;
-    }
-    // if(ev.target.dataset.nav != null){
-    //     console.log(` ── Navigating to: 💠 ${ev.target.dataset.nav} 📍 ── `);
-    //     let navTo = ev.target.dataset.nav;
-    //     if(navTo == "resume") {
-    //         `https://github.com/UserStef/Resume`;
-    //     }
-    // }
-});
 
 window.addEventListener('load', async () =>{
     console.log(" -- Start of load event. -- ");
